2d80e074be8ee8502ac066c2527c50a89d51b405
Chandu-6 <138025522+Chandu-6@users.noreply.github.com>
2023-12-21 18:05:21 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,35 @@
+#Data types
+#created by Chandana p
+#created on 20/12/23
+
+#Integer or number
+marks=20
+print("the marks:",(type(marks)),"the adress of marks:",(id(marks)))
+Marks=marks+10
+print(Marks)
+print("the updated marks:",(type(Marks)),"the adress of upated Marks:",(id(Marks)))
+#Float
+marks=20.5
+print("the marks:",(type(marks)),"the adress of marks:",(id(marks)))
+Marks=marks+20.5
+print("the updated marks:",(type(Marks)),"the adress of updated marks:",(id(Marks)))
+#Complex
+marks=20.30j
+print("the marks:",(type(marks)),"the adress of marks:",(id(marks)))
+#String
+name="chandu"
+print("the name:",(type(name)),"the adress of name:",(id(name)))
+Name='chandu'
+print("the Name:",(type(Name)),"the adress of Name:",(id(Name)))
+adress='''Hyderabad
+Madhapur
+Arunodaya colony'''
+print("the adress:",(type(adress)),"the memory adress of person:",(id(adress)))
+
+_name="chandu"
+print("the Name:",(type(_name)),"the adress of Name:",(id(_name)))
+#Boolean
+Value=True;
+print("the Value:",(type(Value)),"the adress of Value:",(id(Value)))
+value=False;
+print("the Value:",(type(Value)),"the adress of Value:",(id(Value)))
