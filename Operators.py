c86bc6a7b8b5b0fa0692009a9d431499423c578d
Chandu-6 <138025522+Chandu-6@users.noreply.github.com>
2023-12-21 18:00:20 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,132 @@
+# Arithmetic operators
+# Created by Chandana p
+# Created on 21/12/23
+# integers
+num1 = 20
+num2 = 30
+num3 = num1+num2
+print("The addition of two numbers:", num3, type(num3), id(num1), id(num2))
+num3 = num2-num1
+print("The subtraction of two numbers:", num3, type(num3))
+num3 = num1*num2
+print("The multiplication of two numbers:", num3, type(num3))
+num3 = num1/num2
+print("The division of two numbers:", num3, type(num3))
+num3 = num1//num2
+print("The floor of two numbers:", num3, type(num3))
+num3 = num1 % num2
+print("The remainder of two numbers:", num3, type(num3))
+num3 = num1**2
+print("The exponent of two numbers:", num3, type(num3))
+
+
+# Float
+a = 20.5
+b = 10.5
+Add = a+b
+print("The addition of two numbers:", Add, type(Add))
+Sub = a-b
+print("The subtraction of two numbers:", Add, type(Sub))
+Mul = a*b
+print("The multiplication of two numbers:", Add, type(Mul))
+Div = a/b
+print("The division of two numbers:", Add, type(Div))
+Floor = a//b
+print("The floor of two numbers:", Add, type(Floor))
+Remainder = a % b
+print("The remainder of two numbers:", Add, type(Remainder))
+Exponent = a**2
+print("The exponent of :", Add, type(Exponent))
+
+
+# Complex
+num1 = 50.04j
+num2 = 20.10j
+num3 = num1+num2
+print("The addition of two numbers:", num3, type(num3))
+num3 = num1-num2
+print("The subtraction of two numbers:", num3, type(num3))
+num3 = num1*num2
+print("The multiplication of two numbers:", num3, type(num3))
+num3 = num1/num2
+print("The division of two numbers:", num3, type(num3))
+# num3=num1%num2
+# print("The reaminder of two numbers:",num3,type(num3))
+# num3=num1//num2
+# print("The floor of two numbers:",num3,type(num3))
+num3 = num1**2
+print("The exponent of num3:", num3, type(num3))
+
+# Assignment operators
+# Created by chandana p
+# Created on 21/12/23
+
+# Integers
+a = 10
+print("The result of a:", a, type(a), id(a))
+a = 20
+a += 3
+print("The result of a:", a, type(a), id(a))
+b = 30
+b -= 10
+print("The result of b:", b, type(b), id(b))
+c = 40
+c *= 10
+print("The result of c:", c, type(c), id(c))
+d = 20
+d /= 10
+print("The result of d:", d, type(d), id(d))
+e = 30
+e //= 10
+print("The result of e:", e, type(e), id(e))
+f = 10
+f **= 2
+print("The result of f:", f, type(f), id(f))
+
+
+# Float
+a = 1.4
+a += 1.6
+print("The result of a:", a, type(a), id(a))
+a = 1.4
+a -= 1.6
+print("The result of a:", a, type(a), id(a))
+a = 1.4
+a *= 1.6
+print("The result of a:", a, type(a), id(a))
+a = 1.4
+a /= 1.6
+print("The result of a:", a, type(a), id(a))
+a = 1.4
+a //= 1.6
+print("The result of a:", a, type(a), id(a))
+a = 1.4
+a %= 1.6
+print("The result of a:", a, type(a), id(a))
+a = 1.4
+a **= 2
+print("The result of a:", a, type(a), id(a))
+
+
+# Complex
+a = 10.0j
+a += 10.4j
+print("The result of a:", a, type(a), id(a))
+a = 10.0j
+a -= 10.4j
+print("The result of a:", a, type(a), id(a))
+a = 10.0j
+a *= 10.4j
+print("The result of a:", a, type(a), id(a))
+a = 10.0j
+a /= 10.4j
+print("The result of a:", a, type(a), id(a))
+a = 10.0j
+# a//=10.4j
+print("The result of a:", a, type(a), id(a))
+a = 10.0j
+# a%=10.4j
+print("The result of a:", a, type(a), id(a))
+a = 10.0j
+a **= 10
+print("The result of a:", a, type(a), id(a))
