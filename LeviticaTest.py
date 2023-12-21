f85805bbdebea6162259fd7a2affe88450dd4f5b
Chandu-6 <138025522+Chandu-6@users.noreply.github.com>
2023-12-21 18:03:21 +0530

    Add files via upload

---
+++ LeviticaTest.py
@@ -0,0 +1,23 @@
+#find the area of corcle with user input
+#created on 20-12-2023
+#created by chandana p
+'''find the area of circle with user input
+created on 20-12-2023
+created by chandna p'''
+radius=(int(input("enter the radius of the circle(only enter integers)")))
+result=3.142*radius*radius
+print(result)
+#find the area of rectangle
+#Created on 20-12-23
+#Created by chandana p6
+length=(int(input("enter the length of the rectangle(only integers)")))
+width=(int(input("enter the width of the rectangle(only integers)")))
+area=length*width
+print(area)
+#Find the area of triangle
+#Created on 20-12-23
+#Created by chandana p
+base = (int(input("enter the base of triangle(only integers)")))
+height = (int(input("enter the height of triangle(only integers)")))
+area =base*height/2
+print(area)
