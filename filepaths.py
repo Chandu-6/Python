eddcf5d368f36556b5196e7cf216cf0718d31ba2
Chandana60 <138025522+Chandu-6@users.noreply.github.com>
2024-01-09 18:00:55 +0530

    Filepaths
    09/01/24

---
+++ filepaths.py
@@ -0,0 +1,14 @@
+import os
+import shutil
+filepath="c:\\Samples"
+print("the folder or directory is created:")
+os.mkdir(filepath+"\PythonExamples")
+# os.chdir(filepath)
+print("The folder or directory is created:")
+# print("The folder is having:",os.listdir(filepath))
+# filepath="c:\\Samples\\PythonExamples\\read.txt"
+# os.remove(filepath)
+# Sourcepath="c:\\Samples\\Read"
+# destinationpath="c:\\samples\\PythonExamples\\copied.txt"
+# shutil.copy(Sourcepath,destinationpath)
+# print("File copy completed")
