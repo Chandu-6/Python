fac4a5b94775b62469886724dd907f9f71b8823d
Chandana60 <138025522+Chandu-6@users.noreply.github.com>
2024-01-09 17:58:26 +0530

    User inputs
    09/01/24

---
+++ practice9.py
@@ -0,0 +1,24 @@
+#User inputs
+#Created on 09/01/24
+#created by chandana p
+x=input("enter the name")
+print("Name:",x)
+#Adition
+x=input("Enter x value")
+y=input("Enter y value")
+z=x+y
+print(z)
+x=int(input("Enter x value"))
+y=int(input("Enter y value"))
+# a=int(x)
+# b=int(y)
+# z=a+b
+z=x+y
+print(z)
+#
+name="chandana"
+print("Hello\n"*10)
+chandu=0
+while(chandu<15):
+    print("hii",chandu)
+    chandu+=1
