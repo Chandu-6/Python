9906da35a0aae86493c67dfabe95232f71829786
Chandana Potturu <138025522+Chandu-6@users.noreply.github.com>
2024-02-15 17:49:09 +0530

    15/02/24(numpy index)

---
+++ numpyEx2.py
@@ -0,0 +1,17 @@
+import numpy as np
+Twodarray=np.array([[1,2,3],[4,5,6],[7,8,9]])
+# print("The twodarray elements is:",Twodarray)
+# print("The length of twdarray is:",Twodarray.size)
+# print("The dimension of array is:",Twodarray.ndim)
+# print("The datatype of array is:",Twodarray.dtype)
+# print("The shape of array is:",Twodarray.shape)
+
+# res = np.Twodarray([0, 1, 1])
+print(Twodarray[2, 0])
+
+Threedarray=np.array([[[1,2,3],[4,5,6],[7,8,9]]])
+print(Threedarray[0, 2, 1])
+
+Fourdarray=np.array([[[[1.1,2.2,3.3,4.4],[1.2,2.2,3.2,4.2],[1.3,2.3,3.3,4.3]]]])
+print(Fourdarray[0, 0, 2, 2])
+
