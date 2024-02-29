e8a1f50cef4506c9c0e5911d1614cb9af6c298eb
Chandana Potturu <138025522+Chandu-6@users.noreply.github.com>
2024-02-29 16:53:47 +0530

    29/02/24(Creating folders)

---
+++ practice filepaths.py
@@ -0,0 +1,10 @@
+import os
+import shutil
+filepath ="c://chandu"
+print("The folder is created")
+#os.mkdir(filepath)
+#os.mkdir(filepath+"//Examples")
+print("The folder is created")
+# os.chdir(filepath)
+os.mkdir(filepath+"//Examples//Subexamples")
+print("The folder is created")
